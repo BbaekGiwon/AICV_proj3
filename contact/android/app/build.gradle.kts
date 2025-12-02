1eaa916d5d2036e30b47b51246880c260b959f2c
dlrldms <dlrldms0803@gmail.com>
2025-12-02 15:18:30 +0900

    Final

--- contact/android/app/build.gradle.kts
+++ contact/android/app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 
     buildTypes {
         release {
+            isMinifyEnabled = false
+            isShrinkResources = false
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- contact/lib/utils/constants.dart
+++ contact/lib/utils/constants.dart
@@ -1,5 +1,5 @@
-const String AGORA_APP_ID = "fc72b3363009410b8aca359a17879619";
+const String AGORA_APP_ID = "f8a91a0479fa423eb1f4af0cb7b83cf4";
 
 // ⚠️ 토큰은 만료될 수 있으니 필요할 때 Agora 콘솔에서 새로 받아서 교체
 const String AGORA_TOKEN =
-    "007eJxTYHgi8nvH1vn5N3e0bvpn97XUWejsBPl8++s7/u7N/6Y5jfevAkNasrlRkrGxmbGBgaWJoUGSRWJyorGpZaKhuYW5pZmhZWiiXmZDICND+SQHFkYGCATxORiMDIxMDYEEAwMApt4g7Q==";
+    "";
